147e11c7068b945c85acff333846e49a83f1e219
anna-sorts <anna.senen@gmail.com>
2017-08-23 18:52:10 +0200

    4.34 min, fins aquí bé

--- core/Request.php
+++ core/Request.php
@@ -4,8 +4,10 @@
 	{
 		public static function uri()
 		{
-			return trim($_SERVER['REQUEST_URI'], '/');
-			//return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+			//return trim($_SERVER['REQUEST_URI'], '/');
+			return trim(
+				parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+			);
 		}
 
 		public static function method()
